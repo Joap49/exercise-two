c00e194fa56dccb0ecb5cfc3b93fda60686d8a37
Joanna Pilika <joannap@joannas-mbp.myfiosgateway.com>
2020-10-08 15:27:55 -0400

    CSS + updated Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,7 +16,7 @@ const [city, setCity] = useState("Milan");
 
 useEffect(() => {
   axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${"Milan"}&appid=${weatherKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${"Milan"}&units=metric&appid=${weatherKey}`
   )
   .then(function (response) {
     const weather = response.data;
@@ -26,7 +26,6 @@ useEffect(() => {
     // handle error
     console.log(error);
   });
-
 }, [city]);
 
 useEffect(() => {
@@ -43,6 +42,7 @@ useEffect(() => {
 
 const { // list of objects
   cloudiness,
+  cloudinessValue,
   currentTemp,
   highTemp,
   humidity,
@@ -51,6 +51,7 @@ const { // list of objects
   windSpeed
 } = useMemo(() => {
   let cloudiness = '';
+  let cloudinessValue = 0;
   let currentTemp = '';
   let highTemp = '';
   let humidity = '';
@@ -59,16 +60,16 @@ const { // list of objects
   let windSpeed = '';
 
   if(weatherData){
-    console.log("weatherData not null");
-    cloudiness = `${weatherData.clouds.all}%`
-    currentTemp = `${weatherData.main.temp}`
-    highTemp = `${weatherData.main.temp_max}`
-    humidity = `${weatherData.main.humidity}%`; // instead of adding string
-    lowTemp = `${weatherData.main.temp_min}` 
-    weatherType = `${weatherData.weather[0].description}`
+    //console.log("weatherData not null");
+    cloudiness = `${weatherData.clouds.all}%`;
+    cloudinessValue = weatherData.clouds.all;
+    currentTemp = `${Math.round(weatherData.main.temp)}°`;
+    highTemp = `${Math.round(weatherData.main.temp_max)}°`;
+    humidity = `${weatherData.main.humidity}%`; 
+    lowTemp = `${Math.round(weatherData.main.temp_min)}°`;
+    weatherType = `${weatherData.weather[0].description}`;
     windSpeed = `${weatherData.wind.speed} km/h`;
    
-
   }
   
     return { 
@@ -84,39 +85,45 @@ const { // list of objects
 
 console.log("weatherData", weatherData);
 
-  return (
-    <>
-      <Header />
-      <main className="Home">
-        <h2>Weather in { city }</h2>
-          <div className="WeatherInfo">
-            <div className="WeatherInfo_Basic">
-              <div className="WeatherInfo_Image">
-                <weatherImage weatherType={weatherType} />
-              </div>
-              <p>Weather Type: {weatherType}</p>
-              <h3 className="Label">Current Temperature:</h3>
-              <p className="WeatherInfo_Temperature">{currentTemp}</p>
-            </div>
-            <div className="WeatherInfo_Extra">
-              <h3 className="Label">High Temperature:</h3> 
-              <p>{highTemp}</p>
-              <h3 className="Label">Low Termperature:</h3> 
-              <p>{lowTemp}</p>
-              <h3 className="Label">Cloudiness:</h3> 
-              <p>{cloudiness}</p>
-              <h3 className="Label">Humidity:</h3> 
-              <p>{humidity}</p>
-              <h3 className="Label">Wind Speed:</h3> 
-              <p>{windSpeed}</p>
-            </div>
-            
-
+return (
+  <>
+    <Header />
+    <main className="Home">
+      <h2>
+        Weather in <span>{city}</span>
+      </h2>
+      <div className="WeatherInfo">
+        <div
+          className="WeatherInfo_Basic"
+          style={{ backgroundColor: `rgba(0,0,0,${cloudinessValue / 200})` }}
+        >
+          <div className="WeatherInfo_Image">
+            <WeatherImage weatherType={weatherType} />
           </div>
-      </main>
-    </>
-  );
-
+          <p className="WeatherInfo_Type">{weatherType} </p>
+          <h3 className="Label">Current Temperature:</h3>
+          <p className="WeatherInfo_Temperature">{currentTemp}</p>
+        </div>
+        <div className="WeatherInfo_Extra">
+          <div className="WeatherInfo_Extra_Column">
+            <h3 className="Label">High Temperature:</h3>
+            <p className="WeatherInfo_Temperature_Small">{highTemp}</p>
+            <h3 className="Label">Low Temperature:</h3>
+            <p className="WeatherInfo_Temperature_Small">{lowTemp}</p>
+          </div>
+          <div className="WeatherInfo_Extra_Column">
+            <h3 className="Label">Cloudiness: </h3>
+            <p className="WeatherInfo_Temperature_Small">{cloudiness}</p>
+            <h3 className="Label">Humidity:</h3>
+            <p className="WeatherInfo_Temperature_Small">{humidity}</p>
+            <h3 className="Label">Wind Speed:</h3>
+            <p className="WeatherInfo_Temperature_Small"> {windSpeed}</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
+);
 }
 
 export default Home;
